c9068060ea457f5342600db8c2c0731f9b853d47
JessAlcock <jess2121@gmail.com>
2017-09-02 14:24:09 +0800

    New Update TDD

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,19 @@
 from selenium import webdriver
+import unittest
 
-browser =webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in broswer.title 
+	def setUp(self):
+	    self.browser = webdriver.Firefox()
+		
+	def tearDown(self):
+		self.browser.quit()
+	
+	def test_can_start_a_list_and_retrieve_it_later(self):
+	    self.browser.get('http://localhost:8000')
+		
+		# notices the page title and header
+		self.assertIn('Django', self.browser.title)
+
+	if__name__ =='__main__':
+		unittest.main()
